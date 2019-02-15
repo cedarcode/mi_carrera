f284fe2a1c98382921edbdae9122306d9104bba2
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2019-02-15 10:38:33 -0300

    Change the name of the courses helper

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,5 +1,5 @@
 module CoursesHelper
-  def define_initial_state(course)
+  def course_approved?(course)
     return !session[:approved_courses].nil? && session[:approved_courses].include?("#{course.id}")
   end
 end
