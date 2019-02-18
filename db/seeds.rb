8c5d4ccaac4e114a19f495fdfa83408bb3b72989
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2019-02-18 10:54:23 -0300

    fix: show shortnames of the courses with large names in the list

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 [
   ["Cálculo 1", 13],
-  ["Geometría y Álgebra Lineal 1", 9],
+  ["Geometría y Álgebra Lineal 1", 9, "GAL 1"],
   ["Física 1", 10],
   ["Cálculo 2", 16],
-  ["Geometría y Álgebra Lineal 2", 9],
+  ["Geometría y Álgebra Lineal 2", 9, "GAL 2"],
   ["Matemática Discreta 1", 9],
   ["Programación 1", 10],
   ["Probabilidad y Estádistica", 10],
@@ -21,22 +21,22 @@
   ["Economía", 7],
   ["Programación 3", 15],
   ["Arquitectura de Computadoras", 12],
-  ["Politicas Científicas en Información y Computación", 3],
+  ["Politicas Científicas en Información y Computación", 3, "Políticas Científicas en Inf. y Comp."],
   ["Métodos Numéricos", 8],
-  ["Introducción a la Investigación de Operaciones", 10],
+  ["Introducción a la Investigación de Operaciones", 10, "IIO"],
   ["Teoría de Lenguajes", 12],
   ["Sistemas Operativos", 12],
   ["Programación 4", 15],
   ["Ciencia Tecnología y Sociedad", 8],
-  ["Administración General para Ingenieros", 5],
+  ["Administración General para Ingenieros", 5, "AGPI"],
   ["Gestión de Calidad", 6],
   ["Fundamentos de Base de Datos", 15],
   ["Taller de Programación", 15],
-  ["Práctica de Administración para Ingenieros", 5],
+  ["Práctica de Administración para Ingenieros", 5, "PAI"],
   ["Redes de Computadoras", 12],
   ["Control de Calidad", 8],
-  ["Introducción a la Ingeniería de Software", 10],
-  ["Proyecto de Ingeniería de Software", 15]
+  ["Introducción a la Ingeniería de Software", 10, "IIS"],
+  ["Proyecto de Ingeniería de Software", 15, "PIS"]
 ].each do |course_data|
-  Course.where(name: course_data[0]).first_or_create.update!(credits: course_data[1])
+  Course.where(name: course_data[0]).first_or_create.update!(short_name: course_data[2], credits: course_data[1])
 end
