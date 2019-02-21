f0a3ba441294b3ca54bba5fc0cf340d9bf7b3680
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-02-21 16:33:48 -0300

    refactor: prefer functional-style over instance variables

--- app/models/bedel.rb
+++ app/models/bedel.rb
@@ -1,30 +1,31 @@
 class Bedel
   def initialize(session)
     @session = session
+
     @session[:approved_courses] ||= []
     @session[:approved_exams] ||= []
   end
 
   def add_approved_course(subject)
-    @session[:approved_courses] += [subject.id]
+    session[:approved_courses] += [subject.id]
   end
 
   def remove_approved_course(subject)
-    @session[:approved_courses] -= [subject.id]
+    session[:approved_courses] -= [subject.id]
   end
 
   def add_approved_exam(subject)
-    @session[:approved_exams] += [subject.id]
+    session[:approved_exams] += [subject.id]
   end
 
   def remove_approved_exam(subject)
-    @session[:approved_exams] -= [subject.id]
+    session[:approved_exams] -= [subject.id]
   end
 
   def credits
     credits = 0
 
-    @session[:approved_exams].each do |subject_id|
+    session[:approved_exams].each do |subject_id|
       subject = Subject.find(subject_id)
       credits += subject.credits
     end
@@ -33,10 +34,14 @@ class Bedel
   end
 
   def approved_course?(subject)
-    @session[:approved_courses].include?(subject.id)
+    session[:approved_courses].include?(subject.id)
   end
 
   def approved_exam?(subject)
-    @session[:approved_exams].include?(subject.id)
+    session[:approved_exams].include?(subject.id)
   end
+
+  private
+
+  attr_reader :session
 end
