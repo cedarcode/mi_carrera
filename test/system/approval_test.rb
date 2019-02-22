fdb9e80121f89d38c3860ee9caa364fd9dcc1483
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2019-02-22 11:52:25 -0300

    test: add test coverage for removing approvals

--- test/system/approval_test.rb
+++ test/system/approval_test.rb
@@ -45,4 +45,56 @@ class ApprovalTest < ApplicationSystemTestCase
     assert_text "9 créditos"
     assert page.has_checked_field?("checkbox_#{@subject.id}", visible: false)
   end
+
+  test "student remove approved course" do
+    visit subject_path(@subject)
+
+    check "Curso aprobado?", visible: false
+    click_on "arrow_back"
+
+    click_on "GAL 1"
+
+    assert page.has_checked_field?("Curso aprobado?", visible: false)
+
+    uncheck "Curso aprobado?", visible: false
+    click_on "arrow_back"
+
+    click_on "GAL 1"
+
+    assert page.has_unchecked_field?("Curso aprobado?", visible: false)
+  end
+
+  test "student remove approved exam" do
+    visit subject_path(@subject)
+
+    check "Examen aprobado?", visible: false
+    click_on "arrow_back"
+
+    click_on "GAL 1"
+
+    assert page.has_checked_field?("Examen aprobado?", visible: false)
+
+    uncheck "Examen aprobado?", visible: false
+    click_on "arrow_back"
+
+    click_on "GAL 1"
+
+    assert page.has_unchecked_field?("Examen aprobado?", visible: false)
+  end
+
+  test "student remove approved subject" do
+    visit root_path
+
+    find(".mdc-checkbox").click
+
+    visit root_path
+
+    assert page.has_checked_field?("checkbox_#{@subject.id}", visible: false)
+
+    find(".mdc-checkbox").click
+
+    visit root_path
+
+    assert page.has_unchecked_field?("checkbox_#{@subject.id}", visible: false)
+  end
 end
