31b41caecb3624da49a9884a6e99c8d8aa2830cd
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2019-03-01 11:46:24 -0300

    refactor: minor changes in the exam-visibility controller

--- app/javascript/controllers/exam_visibility_controller.js
+++ app/javascript/controllers/exam_visibility_controller.js
@@ -4,10 +4,10 @@ export default class extends Controller {
   static targets = [ "exam" ]
 
   approvalChange(event) {
-    let able_to_enroll_exam = event.detail[0]["able_to_enroll_exam"];
+    let ableToEnrollExam = event.detail[0]["able_to_enroll_exam"];
 
-    this.examTargets.forEach((element, i) => {
-      element.classList.toggle('exam-hidden', !able_to_enroll_exam);
+    this.examTargets.forEach((element) => {
+      element.classList.toggle('exam-hidden', !ableToEnrollExam);
     })
   }
 }
