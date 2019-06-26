d648b49218efb8438094790ebff2f43de68ec099
Facundo Padula <facundo.padula@cedarcode.com>
2019-06-26 13:54:37 -0300

    style: add missing whitespace

--- app/javascript/controllers/prerequisite_controller.js
+++ app/javascript/controllers/prerequisite_controller.js
@@ -6,10 +6,10 @@ export default class extends Controller {
   toggle() {
     if (this.collapsableTarget.style.display == "block") {
       this.collapsableTarget.style.display = "none";
-      this.buttonTarget.innerHTML ="chevron_right"
+      this.buttonTarget.innerHTML = "chevron_right"
     } else {
       this.collapsableTarget.style.display = "block";
-      this.buttonTarget.innerHTML ="expand_more";
+      this.buttonTarget.innerHTML = "expand_more";
     }
   }
 }
