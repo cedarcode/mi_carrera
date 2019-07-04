291a6392bd05d10ef4ac8fe7605fdca64542729b
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-07-04 10:36:21 -0300

    style: fix rubocop offense

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @groups_and_credits = bedel.credits_by_group
     respond_to do |format|
-      format.html { }
+      format.html
     end
   end
 
