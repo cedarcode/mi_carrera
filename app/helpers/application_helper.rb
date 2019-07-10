5be2481d0da13d5269e840fa9fc0cc7b386c29eb
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-07-10 14:07:56 -0300

    fix: fix top-app-bar icon styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_icon(name, path)
+    link_to(name, path, class: "material-icons mdc-top-app-bar__navigation-icon mdc-icon-button")
+  end
+
+  def action_icon(name, path)
+    link_to(name, path, class: "material-icons mdc-top-app-bar__action-item mdc-icon-button")
+  end
 end
