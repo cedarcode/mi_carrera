c9e7e3804f8000e4f0a75861de8322f979633fbd
Facundo Padula <facundo.padula@cedarcode.com>
2019-07-25 14:16:54 -0300

    refactor: move js to javascript packs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require serviceworker-companion
--- app/javascript/serviceworker-companion.js
+++ app/javascript/serviceworker-companion.js
@@ -1,8 +1,5 @@
 if (navigator.serviceWorker) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-      .then(function(reg) {
-        console.log('[Companion]', 'Service worker registered!');
-      });
   });
 }
