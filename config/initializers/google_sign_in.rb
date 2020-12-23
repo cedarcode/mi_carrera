55bff7bb46be7e51a13dd9067dc6c97c0e8ddd73
Juan Arias <juan.arias@cedarcode.com>
2020-12-23 12:53:04 -0300

    more API configuration and making buttons prettier

---
+++ config/initializers/google_sign_in.rb
@@ -0,0 +1,4 @@
+Rails.application.configure do
+  config.google_sign_in.client_id     = ENV['359430346618-1vv418e93688ca6a75n71su6skrj7g5k.apps.googleusercontent.com']
+  config.google_sign_in.client_secret = ENV['dMe1AAP3rh95i_u26q2IE3fu']
+end
