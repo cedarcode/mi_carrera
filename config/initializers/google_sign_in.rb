fe92caaf249f67275d80d6f51cacafc25faf8fdd
Juan Arias <juan.arias@cedarcode.com>
2020-12-23 15:34:49 -0300

    Dotenv configuration and finishing google sign in

--- config/initializers/google_sign_in.rb
+++ config/initializers/google_sign_in.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
-  config.google_sign_in.client_id     = ENV['359430346618-1vv418e93688ca6a75n71su6skrj7g5k.apps.googleusercontent.com']
-  config.google_sign_in.client_secret = ENV['dMe1AAP3rh95i_u26q2IE3fu']
+  Dotenv.load
+
+  config.google_sign_in.client_id = ENV['GOOGLE_SIGN_IN_CLIENT_ID']
+  config.google_sign_in.client_secret = ENV['GOOGLE_SIGN_IN_CLIENT_SECRET']
 end
