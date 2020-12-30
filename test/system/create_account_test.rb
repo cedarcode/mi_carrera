52ae8d1337369048db123f59b4ffd3d8d0ae9207
Juan Arias <juan.arias@cedarcode.com>
2020-12-30 12:03:07 -0300

    Fix on create account test

--- test/system/create_account_test.rb
+++ test/system/create_account_test.rb
@@ -1,11 +1,6 @@
 require "application_system_test_case"
 
 class CreateAccountTest < ApplicationSystemTestCase
-  setup do
-    @maths = create_group(name: "Matemáticas")
-    @subject = create_subject(name: "GAL 1", credits: 9, group: @maths)
-  end
-
   test "user can see a google sign in button" do
     visit root_path
     click_on "person"
