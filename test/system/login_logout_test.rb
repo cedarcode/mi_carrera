7f0a57c120476e53785eaece238150782d836eca
Juan Arias <juan.arias@cedarcode.com>
2020-12-30 14:15:39 -0300

    Login tests

--- test/system/login_logout_test.rb
+++ test/system/login_logout_test.rb
@@ -1,20 +1,6 @@
 require "application_system_test_case"
 
 class LoginLogoutTest < ApplicationSystemTestCase
-  setup do
-    @user = User.new(name: "Test User", email_address: "test.user@cedarcode.com")
-  end
-
-  test "logged in user can see link to log out" do
-    visit root_path
-    session[:user_id] = @user.id
-    click_on "person"
-
-    assert_selector "a", text: 'Cerrar sesión'
-    click_on "Cerrar sesión"
-    assert_equal nil, session[:user_id]
-  end
-
   test "user can see log in icon and google log in button" do
     visit root_path
     click_on "person"
@@ -22,5 +8,12 @@ class LoginLogoutTest < ApplicationSystemTestCase
 
     assert_text "Iniciar sesión"
     assert_selector "button", text: 'Iniciar sesión con Google'
+  end
+
+  test "not logged in user cant see log out link" do
+    visit root_path
+    click_on "person"
+
+    assert_no_text "Cerrar sesión"
   end
 end
