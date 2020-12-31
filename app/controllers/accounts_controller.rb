a9afb7f86572d13ffceff723da35e1290ab093db
Juan Arias <juan.arias@cedarcode.com>
2020-12-31 12:16:49 -0300

    Changes from review comments on PR

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,8 +3,9 @@ class AccountsController < ApplicationController
   end
 
   def create_callback
-    login = GoogleSignIn::Identity.new(flash[:google_sign_in]["id_token"])
-    user = User.new(name: login.name, email_address: login.email_address, avatar_url: login.avatar_url)
+    google_identity = GoogleSignIn::Identity.new(flash[:google_sign_in]["id_token"])
+    user = User.new(name: google_identity.name, email_address: google_identity.email_address,
+                    avatar_url: google_identity.avatar_url)
     if user.save
       session[:user_id] = user.id
       redirect_to root_path
