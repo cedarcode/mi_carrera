ddcb36d511e9402e29822599317d1f0ad15d46fe
Juan Arias <juan.arias@cedarcode.com>
2021-01-11 11:23:29 -0300

    Handling turbolinks cache and fixing text's responsive issues

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -11,5 +11,10 @@ export default class extends Controller {
     screen.setAttribute("id", "menu-background");
     screen.setAttribute("onclick", "document.getElementById('menu-background').remove()");
     document.querySelector(".mdc-menu-surface--anchor").prepend(screen);
+    document.addEventListener("turbolinks:before-cache", function() {
+      if (document.getElementById('menu-background') != null) {
+        document.getElementById('menu-background').remove();
+      }
+    })
   }
 }
