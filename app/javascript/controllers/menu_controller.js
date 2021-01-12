57b35d8603e9cc5e2a50feae63cb6e475a439c27
Juan Arias <juan.arias@cedarcode.com>
2021-01-12 15:29:00 -0300

    Fix on menu issue and moving it to a partial

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -4,17 +4,24 @@ import {MDCMenu} from '@material/menu';
 export default class extends Controller {
   static targets = ["content"]
 
-  openMenu(){
-    const menu = new MDCMenu(this.contentTarget);
-    menu.open = true;
+  connect() {
+    this.menu = new MDCMenu(this.contentTarget);
+    this.menu.open = false;
+  }
+
+  openMenu() {
+    this.menu.open = true;
     const screen = document.createElement("div");
     screen.setAttribute("id", "menu-background");
-    screen.setAttribute("onclick", "document.getElementById('menu-background').remove()");
+    screen.addEventListener("click", function() {
+      screen.remove();
+    });
     document.querySelector(".mdc-menu-surface--anchor").prepend(screen);
     document.addEventListener("turbolinks:before-cache", function() {
-      if (document.getElementById('menu-background') != null) {
-        document.getElementById('menu-background').remove();
+      const screen = document.getElementById('menu-background');
+      if (screen != null) {
+            screen.remove();
       }
-    })
+    });
   }
 }
