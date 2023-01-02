fc6bc4ffa8059e997ad540dda40cf7c6ae0defb5
Renzo Minelli <renminelli@gmail.com>
2023-01-02 10:39:57 -0300

    chore: added whitespace

--- test/models/bedel_test.rb
+++ test/models/bedel_test.rb
@@ -118,6 +118,7 @@ class BedelTest < ActiveSupport::TestCase
     assert_equal([], user.approvals[:approved_exams])
     assert_equal([], store[:approved_exams])
   end
+
   test "when bedel was instantiated without a user, .add_approval when receiving a course that can't approve, should do nothing" do
     store = { approved_courses: [@subject1.id] }
     bedel = Bedel.new(store)
