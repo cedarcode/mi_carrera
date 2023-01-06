ac1078e35c85161ca381e4661b313e9d8824c37f
Renzo Minelli <renminelli@gmail.com>
2023-01-06 14:54:07 -0300

    refactor: use targets in controller

--- app/javascript/controllers/info_dialog_controller.js
+++ app/javascript/controllers/info_dialog_controller.js
@@ -2,8 +2,11 @@ import { Controller } from "stimulus";
 import { MDCDialog } from "@material/dialog";
 
 export default class extends Controller {
+  static targets = ["trigger", "dialog"];
+
   open() {
-    const dialog = new MDCDialog(document.querySelector(".mdc-dialog"));
+    const dialog = new MDCDialog(this.dialogTarget);
     dialog.open();
+    this.triggerTarget.blur();
   }
 }
