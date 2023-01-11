dcaeb5037e6193a465afc8ba10817fa3ef309bf3
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-11 19:01:54 -0300

    Fix Rails/InverseOf

--- app/models/subject_group.rb
+++ app/models/subject_group.rb
@@ -1,5 +1,5 @@
 class SubjectGroup < ApplicationRecord
-  has_many :subjects, foreign_key: :group_id
+  has_many :subjects, foreign_key: :group_id, inverse_of: :group
 
   validates :name, presence: true
   validates :code, uniqueness: true
