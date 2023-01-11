a1f87f66c884fc616ab00f941640edaa6d6296f1
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-11 19:49:16 -0300

    Fix Rails/HasManyOrHasOneDependent

--- app/models/subject_group.rb
+++ app/models/subject_group.rb
@@ -1,5 +1,5 @@
 class SubjectGroup < ApplicationRecord
-  has_many :subjects, foreign_key: :group_id, inverse_of: :group
+  has_many :subjects, foreign_key: :group_id, inverse_of: :group, dependent: :restrict_with_exception
 
   validates :name, presence: true
   validates :code, uniqueness: true
