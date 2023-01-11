1d75be1bf6c7125163c335a48b8cc1895d762a63
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-11 19:27:14 -0300

    Ignore some offenses in existing migration

--- db/migrate/20190315153603_create_prerequisite_tree.rb
+++ db/migrate/20190315153603_create_prerequisite_tree.rb
@@ -1,6 +1,6 @@
 class CreatePrerequisiteTree < ActiveRecord::Migration[5.2]
   def change
-    create_table :prerequisites do |t|
+    create_table :prerequisites do |t| # rubocop:disable Rails/CreateTableWithTimestamps
       t.string  :type, null: false
       t.integer :parent_prerequisite_id
       t.integer :dependency_item_id
@@ -13,8 +13,8 @@ class CreatePrerequisiteTree < ActiveRecord::Migration[5.2]
       t.integer :dependency_item_needed_id
     end
 
-    drop_table :dependencies
+    drop_table :dependencies # rubocop:disable Rails/ReversibleMigration
 
-    drop_table :credits_prerequisites
+    drop_table :credits_prerequisites # rubocop:disable Rails/ReversibleMigration
   end
 end
