61d960f5fdadd59de36aa829d6571f4f60281eaa
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-11 19:44:14 -0300

    Fix Rails/Exit

--- config/initializers/yarn.rb
+++ config/initializers/yarn.rb
@@ -1,4 +1,4 @@
 # Ensure JS packages up to date when starting Rails server in development
 if Rails.env.development? && Rails.const_defined?('Server')
-  system("bin/yarn install --check-files") || exit(1)
+  raise "Error installing yarn packages" unless system("bin/yarn install --check-files")
 end
