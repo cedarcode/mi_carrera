b1c8b1e86167b64ccb80865ee6d3231b77a15f5a
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-12 19:02:04 -0300

    Configure rails-ujs

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,4 +3,5 @@
 pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
