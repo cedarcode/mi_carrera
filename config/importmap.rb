b91275316daa0b385e6c4b61b87f708f49446e1f
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-12 19:38:00 -0300

    Import serviceworker-companion and install js files

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "install", to: "install.js"
+pin "serviceworker-companion", to: "serviceworker-companion.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
