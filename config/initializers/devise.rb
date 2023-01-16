4b2db708f1bf845d00776738aba68562e86a10e5
Renzo Minelli <renminelli@gmail.com>
2023-01-16 16:05:47 -0300

    feat: change dotenv for rails credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,5 +311,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
+  config.omniauth :google_oauth2,
+                  Rails.application.credentials.google[:oauth_client_id],
+                  Rails.application.credentials.google[:oauth_client_secret]
 end
