06a3c085158a7857eebeff47da37e096ef108b74
Renzo Minelli <renminelli@gmail.com>
2023-01-16 12:47:54 -0300

    fix: add from email

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,5 +2,6 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: 587,
   user_name: 'apikey',
-  password: ENV['SENDGRID_API_KEY']
+  password: ENV['SENDGRID_API_KEY'],
+  from: 'renzo.minelli@cedarcode.com'
 }
