4d38789ffc768ff00f5a33f8d2dce31798683d00
Renzo Minelli <renminelli@gmail.com>
2023-01-16 14:19:49 -0300

    fix: move default from mail to application_mailer

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,6 +2,5 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: 587,
   user_name: 'apikey',
-  password: ENV['SENDGRID_API_KEY'],
-  from: 'renzo.minelli@cedarcode.com'
+  password: ENV['SENDGRID_API_KEY']
 }
