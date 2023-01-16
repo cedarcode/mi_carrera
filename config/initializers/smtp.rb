010ff8f6322e39ba6c113939781a372f256b6825
Renzo Minelli <renminelli@gmail.com>
2023-01-16 12:08:46 -0300

    feat: add sendgrid configuration

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+  domain: 'http://student.cedarcode.com',
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  user_name: 'apikey',
+  password: ENV['SENDGRID_API_KEY']
+}
