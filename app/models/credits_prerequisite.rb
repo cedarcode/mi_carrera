79a18599e8fe801c8ca4cf70d82bfa5bba4b6e1d
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-18 13:16:02 -0300

    Prerequisite#met?

--- app/models/credits_prerequisite.rb
+++ app/models/credits_prerequisite.rb
@@ -2,4 +2,11 @@ class CreditsPrerequisite < Prerequisite
   belongs_to :subject_group, optional: true
 
   validates :credits_needed, presence: true
+
+  def met?(approved_courses, approved_exams)
+    subjects = subject_group ? subject_group.subjects : Subject.all
+
+    approved_credits = subjects.approved_credits(approved_courses, approved_exams)
+    approved_credits >= credits_needed
+  end
 end
--- app/models/prerequisite.rb
+++ app/models/prerequisite.rb
@@ -1,4 +1,8 @@
 class Prerequisite < ApplicationRecord
   belongs_to :parent_prerequisite, class_name: 'Prerequisite', optional: true
   belongs_to :approvable, optional: true
+
+  def met?(approved_courses, approved_exams)
+    raise NotImplementedError
+  end
 end
