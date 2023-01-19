578f98f09dc2e951f29a8cf5249a6014dd207673
Renzo Minelli <renminelli@gmail.com>
2023-01-19 16:23:01 -0300

    build: remove config not present in rails 7

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'rails/test_help'
 require 'minitest/mock'
 
 class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
