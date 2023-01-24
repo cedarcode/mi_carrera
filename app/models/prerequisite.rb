906cd145c96dc37bd302a3449fc50c14cdf3fe14
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-24 18:22:10 -0300

    Update Prerequisite to use only one list of approved_approvable_ids

--- app/models/prerequisite.rb
+++ app/models/prerequisite.rb
@@ -2,7 +2,7 @@ class Prerequisite < ApplicationRecord
   belongs_to :parent_prerequisite, class_name: 'Prerequisite', optional: true
   belongs_to :approvable, optional: true
 
-  def met?(_approved_courses, _approved_exams)
+  def met?(_approved_approvable_ids)
     raise NotImplementedError
   end
 end
