932b1ecd77fa93e393bdc83e7c0fa6550f5a670c
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-24 19:53:13 -0300

    Avoid using querySelector in credits_counter_controller

--- app/javascript/controllers/credits_counter_controller.js
+++ app/javascript/controllers/credits_counter_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  update(event) {
-    let credits = event.detail;
-
-    document.querySelector(".js-credits-count").innerHTML = credits;
+  update({ detail: credits }) {
+    this.element.innerHTML = credits;
   }
 }
