dd980cb74d088a4853a53b19c302612c29342ea8
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-26 10:18:16 -0300

    Disable Turbo drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs";
 import "install";
 import "serviceworker-companion";
 
 Rails.start();
+
+Turbo.session.drive = false
