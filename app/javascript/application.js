9c31a963d338cae7d02f2f60c7d3e92c28f2c73f
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-01-26 10:18:16 -0300

    bin/rails turbo:install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs";
 import "install";
