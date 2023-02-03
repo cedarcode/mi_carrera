21ac8b050126845a56376fb04ff121518e049fa6
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-02-03 19:33:31 -0300

    Unpin @material/dialog

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
-pin "@material/dialog", to: "https://ga.jspm.io/npm:@material/dialog@14.0.0/dist/mdc.dialog.js"
 pin "@material/drawer", to: "https://ga.jspm.io/npm:@material/drawer@14.0.0/dist/mdc.drawer.js"
 pin "@material/textfield", to: "https://ga.jspm.io/npm:@material/textfield@14.0.0/dist/mdc.textfield.js"
 pin "@material/snackbar", to: "https://ga.jspm.io/npm:@material/snackbar@14.0.0/dist/mdc.snackbar.js"
