c67c7f707d6b1a57faaade608a89be22a25ebdff
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-02-06 14:23:04 -0300

    Disable checkboxes while processing request

--- app/javascript/controllers/foreground_layer_controller.js
+++ app/javascript/controllers/foreground_layer_controller.js
@@ -1,12 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["checkbox"];
+
   add() {
-    var foregroundLayer = document.createElement("div");
-    foregroundLayer.className = "foreground-layer";
-    foregroundLayer.addEventListener("click", function(event) {
-      event.preventDefault();
-    });
-    this.element.append(foregroundLayer);
+    for (const checkbox of this.checkboxTargets) {
+      checkbox.disabled = true;
+    }
   }
 }
