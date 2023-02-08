27ed25dfaa46d7a45013bc089f7e1c9b17913641
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-02-08 18:30:42 -0300

    Include factorybot methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ require 'rails/test_help'
 require 'minitest/mock'
 
 class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
