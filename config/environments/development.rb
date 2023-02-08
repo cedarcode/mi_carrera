c8ed6c8fa172f99df564f45e5df3f9783edc169d
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2023-02-08 11:00:52 -0300

    Remove action_cable code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,9 +62,6 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
