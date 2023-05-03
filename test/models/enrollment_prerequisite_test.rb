aa84c142133f55541cafdfccf987c6962d4b8691
Renzo Minelli <renminelli@gmail.com>
2023-05-03 10:58:20 -0300

    refactor: unify tests

--- test/models/enrollment_prerequisite_test.rb
+++ test/models/enrollment_prerequisite_test.rb
@@ -1,15 +1,10 @@
 require 'test_helper'
 
 class EnrollmentPrerequisiteTest < ActiveSupport::TestCase
-  test "#met? returns false when subject course is approved" do
-    subject_needed = create :subject
-    prerequisite = create :enrollment_prerequisite, approvable_needed: subject_needed.course
-    assert_not prerequisite.met?([subject_needed.course.id])
-  end
-
-  test "#met? returns false when subject course is not approved" do
+  test "#met? always returns false" do
     subject_needed = create :subject
     prerequisite = create :enrollment_prerequisite, approvable_needed: subject_needed.course
     assert_not prerequisite.met?([])
+    assert_not prerequisite.met?([subject_needed.course.id])
   end
 end
