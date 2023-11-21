0129d710f4d18a39f1b44952026065593d199116
Emilio <emilio.cristalli@cedarcode.com>
2023-11-21 17:58:31 -0300

    Fix deprecation warning
    DEPRECATION WARNING: Passing the class as positional argument is deprecated and will be removed in Rails 7.2.
    
    Please pass the class as a keyword argument:
    
      serialize :approvals, type: Array
     (called from <class:User> at ~/mi_carrera/app/models/user.rb:6)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  serialize :approvals, Array
+  serialize :approvals, type: Array
 
   def self.from_omniauth(auth, cookie)
     # check that user with same email exists
