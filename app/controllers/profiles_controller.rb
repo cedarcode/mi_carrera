9bcf37427d0cf696c4684390573fbb2f71624f0c
Renzo Minelli <renminelli@gmail.com>
2023-12-21 19:37:29 -0300

    fix: set correct instance variable

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,8 +9,8 @@ class ProfilesController < ApplicationController
       }
     end
 
-    @groups_credits_met = @groups_and_credits.all? { |group_and_credits| group_and_credits[:credits_needed_reached] }
-    @total_credits = @groups_and_credits.pluck(:credits).reduce :+
+    @all_group_credits_met = @groups_and_credits.all? { |group_and_credits| group_and_credits[:credits_needed_reached] }
+    @total_credits = @groups_and_credits.pluck(:credits).sum
 
     respond_to do |format|
       format.html
