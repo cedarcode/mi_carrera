344e44fc6afcab51eec92bf9add1b7c35e30964f
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-03-04 11:55:33 -0300

    feat: focus on search input upon clicking in search icon

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["appBar", "searchbar"];
+  static targets = ["appBar", "searchbar", "searchInput"];
 
   toggle() {
     this.appBarTarget.classList.toggle("d-none")
     this.searchbarTarget.classList.toggle("d-none")
+    if (!this.searchbarTarget.classList.contains("d-none")) {
+      this.searchInputTarget.focus()
+    }
   }
 }
