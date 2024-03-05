fa1f2a509f051d772754c8ea1826d899d36e4c1d
Renzo Minelli <renminelli@gmail.com>
2024-03-05 19:45:44 -0300

    chore: remove unnecessary format

--- app/controllers/current_optional_subjects_controller.rb
+++ app/controllers/current_optional_subjects_controller.rb
@@ -1,9 +1,5 @@
 class CurrentOptionalSubjectsController < ApplicationController
   def index
     @subjects = Subject.current_semester_optionals.order(:code)
-
-    respond_to do |format|
-      format.html
-    end
   end
 end
