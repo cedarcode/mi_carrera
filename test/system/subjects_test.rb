9a44474dda4795b10b09780c50d554b01009b7e6
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-03-07 18:11:05 -0300

    test: add case for searching subjects for their short name

--- test/system/subjects_test.rb
+++ test/system/subjects_test.rb
@@ -11,44 +11,50 @@ class SubjectsTest < ApplicationSystemTestCase
       create(:subject_prerequisite, approvable_needed: gal1.exam),
     ]
 
-    create :subject, name: "Taller", credits: 11
+    create :subject, name: "Taller 1", short_name: 'T1', credits: 11
 
     visit all_subjects_path
 
     assert_text "GAL 1"
     assert_text "GAL 2"
-    assert_text "Taller"
+    assert_text "T1"
 
     click_search_trigger
     fill_in 'search', with: "Taller\n"
 
     assert_no_text "GAL 1"
     assert_no_text "GAL 2"
-    assert_text "Taller"
+    assert_text "T1"
 
     fill_in 'search', with: " Taller\n"
 
     assert_no_text "GAL 1"
     assert_no_text "GAL 2"
-    assert_text "Taller"
+    assert_text "T1"
 
     fill_in 'search', with: "Taller \n"
 
     assert_no_text "GAL 1"
     assert_no_text "GAL 2"
-    assert_text "Taller"
+    assert_text "T1"
+
+    fill_in 'search', with: "T1\n"
+
+    assert_no_text "GAL 1"
+    assert_no_text "GAL 2"
+    assert_text "T1"
 
     fill_in 'search', with: "This subject does not exist\n"
 
     assert_no_text "GAL 1"
     assert_no_text "GAL 2"
-    assert_no_text "Taller"
+    assert_no_text "T1"
     assert_text "No hay resultados"
 
     fill_in 'search', with: " \n"
 
     assert_text "GAL 1"
     assert_text "GAL 2"
-    assert_text "Taller"
+    assert_text "T1"
   end
 end
