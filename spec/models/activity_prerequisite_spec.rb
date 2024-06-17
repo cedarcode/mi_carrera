53fce2f9fd4123f135bd841f88c2e11ba60efeaa
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-06-17 19:25:28 -0300

    test: use mocks for set up an available and unavailable subject

--- spec/models/activity_prerequisite_spec.rb
+++ spec/models/activity_prerequisite_spec.rb
@@ -5,19 +5,17 @@ RSpec.describe ActivityPrerequisite, type: :model do
     context 'when the approvable needed is available' do
       it 'returns true' do
         subject_needed = create :subject, :with_exam
-        create :credits_prerequisite, approvable: subject_needed.exam, credits_needed: 5
-
+        allow(subject_needed.exam).to receive(:available?).and_return(true)
         prerequisite = create :activity_prerequisite, approvable_needed: subject_needed.exam
 
-        expect(prerequisite.met?([create(:subject, credits: 5).course])).to be_truthy
+        expect(prerequisite.met?([])).to be_truthy
       end
     end
 
     context 'when the approvable needed is not available' do
       it 'returns false' do
         subject_needed = create :subject, :with_exam
-        create :credits_prerequisite, approvable: subject_needed.exam, credits_needed: 5
-
+        allow(subject_needed.exam).to receive(:available?).and_return(false)
         prerequisite = create :activity_prerequisite, approvable_needed: subject_needed.exam
 
         expect(prerequisite.met?([])).to be_falsey
