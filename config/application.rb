da1be036cb1d900b2afa44875386a7c3ae4bedbe
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-08-13 15:00:40 -0300

    build: update rails configs

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module MiCarrera
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks scraper))
+    config.autoload_lib(ignore: %w[assets tasks scraper])
 
     # Configuration for the application, engines, and railties goes here.
     #
