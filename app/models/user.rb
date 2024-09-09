6333ba9e267d1d4c7c24fa7de782f3af907ff8fd
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2024-09-09 15:16:30 -0300

    Enable Rails 7.2 defaults

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  serialize :approvals, type: Array
+  serialize :approvals, type: Array, coder: YAML
 
   def self.from_omniauth(auth, cookie)
     # check that user with same email exists
