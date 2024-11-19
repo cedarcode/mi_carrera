8c024ca02f3a1c5f955862bf71fa7b2a222c83f3
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2024-11-19 19:41:04 -0300

    feat: set minimum browser versions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
   helper_method :current_student
 
   private
