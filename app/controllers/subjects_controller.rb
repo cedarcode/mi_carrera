3c3cec5d16d60fee955399040e58a09560d9c9b0
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2024-11-19 22:54:40 -0300

    feat: split subjects between planned and not planned

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -24,7 +24,12 @@ class SubjectsController < ApplicationController
   end
 
   def planner
-    @subjects = TreePreloader.new.preload
+    @planned_subjects = TreePreloader.new.preload.select do |subject|
+      current_student.approved?(subject.course) ||
+        current_user.planned_subjects.any? { |planned_subject| planned_subject.subject_id == subject.id }
+    end
+
+    @not_planned_subjects = TreePreloader.new.preload - @planned_subjects
   end
 
   private
