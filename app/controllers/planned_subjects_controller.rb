a4a397608a31d72c76894691a91b738daa8076eb
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2024-11-22 11:51:38 -0300

    feat: use turbo_streams to avoid loading the page

--- app/controllers/planned_subjects_controller.rb
+++ app/controllers/planned_subjects_controller.rb
@@ -1,5 +1,14 @@
 class PlannedSubjectsController < ApplicationController
   def create
     current_user.planned_subjects.create(subject_id: params[:subject_id])
+
+    @planned_subjects = TreePreloader.new.preload.select do |subject|
+      current_student.approved?(subject.course) ||
+        current_user.planned_subjects.any? { |planned_subject| planned_subject.subject_id == subject.id }
+    end
+
+    @not_planned_subjects = TreePreloader.new.preload - @planned_subjects
+
+    render :create
   end
 end
