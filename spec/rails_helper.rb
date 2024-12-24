b902b2d8c42e8b8363a533fb24e5d9d2a4654b2d
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2024-12-24 12:45:16 -0300

    test: scope Devise's fix to just system specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :system
 
   # TODO Remove when Devise fixes https://github.com/heartcombo/devise/issues/5705
-  config.before(:each) do
+  config.before(:each, type: :system) do
     Rails.application.reload_routes_unless_loaded
   end
 end
