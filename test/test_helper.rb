0739364420a753f0d9bf66d90967791d6db550f9
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2024-12-30 15:08:25 -0300

    add tests for show password feature

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'minitest/mock'
+Dir[Rails.root.join("test/support/*")].each { |f| require f }
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
