0a24d504557e862d0a07827c3c31cc50e328c481
PaoloCappelli <paolo.cappelli@betterup.co>
2025-01-21 18:59:47 -0300

    Revert "Test average rating update"
    This reverts commit a0ac8a11f14338b2db77d703562ed7d361350bd0.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -17,33 +17,4 @@ RSpec.describe Review, type: :model do
     it { is_expected.to validate_presence_of(:rating) }
     it { is_expected.to validate_inclusion_of(:rating).in_range(1..5) }
   end
-
-  describe 'callbacks' do
-    context 'calls update_rating on subject after commit' do
-      let(:review) { build :review }
-
-      it 'calls update_rating on subject after create' do
-        expect(review.subject).to receive(:update_rating)
-        review.save!
-      end
-
-      it 'calls update_rating on subject after update' do
-        review.save!
-        expect(review.subject).to receive(:update_rating)
-        review.update!(rating: 5)
-      end
-
-      it 'calls update_rating on subject after destroy' do
-        review.save!
-        expect(review.reload.subject).to receive(:update_rating)
-        review.destroy!
-      end
-
-      it 'does not call update_rating on subject if rating was not changed' do
-        review.save!
-        expect(review.subject).not_to receive(:update_rating)
-        review.update!(rating: review.rating)
-      end
-    end
-  end
 end
