1948d2debe285613cda1c827004d4b9d7532a1fd
Nicolas Buero <nicolas.buero@cedarcode.com>
2025-01-21 21:43:57 -0300

    refactor: Usar change_table en vez de change_column por separado

--- db/migrate/20250121233816_add_lockable_to_devise.rb
+++ db/migrate/20250121233816_add_lockable_to_devise.rb
@@ -1,8 +1,9 @@
 class AddLockableToDevise < ActiveRecord::Migration[8.0]
   def change
-    # Only if lock strategy is :failed_attempts
-    add_column :users, :failed_attempts, :integer, default: 0, null: false
-
-    add_column :users, :locked_at, :datetime
+    change_table :users, bulk: true do |t|
+      # Only if lock strategy is :failed_attempts
+      t.integer :failed_attempts, default: 0, null: false
+      t.datetime :locked_at
+    end
   end
 end
