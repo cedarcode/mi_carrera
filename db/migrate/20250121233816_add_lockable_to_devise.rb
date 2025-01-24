a674048eba3d3ec367218318901688a94342e3bf
Nicolas Buero <nicolas.buero@cedarcode.com>
2025-01-24 12:52:29 -0300

    feature: Agregar columnas para soportar el unlock flow con mail

--- db/migrate/20250121233816_add_lockable_to_devise.rb
+++ db/migrate/20250121233816_add_lockable_to_devise.rb
@@ -4,6 +4,12 @@ class AddLockableToDevise < ActiveRecord::Migration[8.0]
       # Only if lock strategy is :failed_attempts
       t.integer :failed_attempts, default: 0, null: false
       t.datetime :locked_at
+
+      # Only if unlock strategy is :email or :both
+      t.string :unlock_token
+
+      # Add index to unlock_token
+      t.index :unlock_token, unique: true
     end
   end
 end
