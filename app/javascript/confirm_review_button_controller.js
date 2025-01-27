63ea6c70ea25699264d9a8fd9eb0bcdabbfede48
Geronimo <geronimo.mendez@ext.airbnb.com>
2025-01-27 16:59:12 -0300

    wip disable button

---
+++ app/javascript/confirm_review_button_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["button", "textarea", "stars"];
+
+  // when user has written some text in the textarea and has also selected a rating
+  // then the button should be enabled.
+
+  initialize() {
+    disable();
+  }
+
+  enable() {
+    console.log('enabling button');
+    this.buttonTarget.disabled = false;
+    this.buttonTarget.style.backgroundColor = "#6804ec";
+    this.buttonTarget.style.color = "white";
+  }
+
+  disable() {
+    console.log('disabling button');
+    this.buttonTarget.disabled = true;
+    this.buttonTarget.style.backgroundColor = "white";
+    this.buttonTarget.style.color = "black";
+  }
+
+  connect() {
+    console.log("confirm_review_button controller connected");
+  }
+}
