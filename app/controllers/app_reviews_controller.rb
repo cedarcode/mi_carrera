6ba7795acec1f96623db3c3f52207c4c001c53e6
Geronimo Mendez <geronimo.mendez@cedarcode.com>
2025-02-09 14:37:18 -0300

    clean up `app_review_controller`

--- app/controllers/app_reviews_controller.rb
+++ app/controllers/app_reviews_controller.rb
@@ -1,22 +1,19 @@
 class AppReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    if !current_user && !localhost
-      redirect_to new_user_session_path, alert: "Inicia sesión para dejar una reseña"
-    else
-      if app_review_params[:content].blank? || app_review_params[:rating].blank?
-        redirect_to root_path, alert: "Debes elegir una calificación y escribir una opinión"
-      end
+    params = app_review_params
 
-      @app_review = AppReview.new(app_review_params)
-
-      if localhost
-        current_user = User.first_or_create(email: 'test@example.com', password: 'password')
-      end
-
-      @app_review.user = current_user
-      @app_review.save
-      redirect_to root_path, notice: "Reseña creada con éxito"
+    if params[:content].blank? || params[:rating].blank?
+      puts 'redirigiendo a root_path'
+      redirect_to root_path, alert: "Debes elegir una calificación y escribir una opinión"; return
     end
+
+    @app_review = AppReview.new(params)
+    @app_review.user = current_user
+    @app_review.save
+
+    redirect_to root_path, notice: "Reseña creada con éxito"
   end
 
   def index
@@ -27,9 +24,5 @@ class AppReviewsController < ApplicationController
 
   def app_review_params
     params.require(:app_review).permit(:content, :rating)
-  end
-
-  def localhost
-    request.host == 'localhost'
   end
 end
