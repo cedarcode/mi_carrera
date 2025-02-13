576de493e0b2320e1e6f4cd2ce6fef99613d8611
Geronimo Mendez <geronimo.mendez@cedarcode.com>
2025-02-13 20:27:49 -0300

    send mails on create for reviews

--- app/models/app_review.rb
+++ app/models/app_review.rb
@@ -1,4 +1,10 @@
 class AppReview < ApplicationRecord
   belongs_to :user
   attr_accessor :user
+
+  after_create :send_review_email
+
+  def send_review_email
+    AppReviewsNotifierMailer.notify_new_review(self).deliver_now
+  end
 end
