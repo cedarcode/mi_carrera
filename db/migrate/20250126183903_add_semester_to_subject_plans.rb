8e652b8f57541a102de722625a8f18d6b3c937ee
santiagoguridi <santiago.guridi@cedarcode.com>
2025-02-13 22:43:02 -0300

    changed to nullable column

--- db/migrate/20250126183903_add_semester_to_subject_plans.rb
+++ db/migrate/20250126183903_add_semester_to_subject_plans.rb
@@ -1,5 +1,5 @@
 class AddSemesterToSubjectPlans < ActiveRecord::Migration[8.0]
   def change
-    add_column :subject_plans, :semester, :integer
+    add_column :subject_plans, :semester, :integer, null: false # rubocop:disable Rails/NotNullColumn
   end
 end
