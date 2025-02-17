23256b5e3cba50d3f077467da341c93f76a752ff
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-02-17 14:27:18 -0300

    feat: use material UI select

---
+++ app/javascript/controllers/select_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus";
+import { MDCMenu } from '@material/menu';
+import { MDCSelect } from '@material/select';
+
+export default class extends Controller {
+  static targets = ["menu", "select"];
+
+  connect() {
+    this.select = new MDCSelect(this.selectTarget);
+    this.menu = new MDCMenu(this.menuTarget);
+  }
+}
