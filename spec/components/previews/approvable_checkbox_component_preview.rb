2ea963ffada58270f7c767682dda64d1319f75b9
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-04-28 18:52:16 -0300

    feat: show different checkbox states in preview

--- spec/components/previews/approvable_checkbox_component_preview.rb
+++ spec/components/previews/approvable_checkbox_component_preview.rb
@@ -1,8 +1,26 @@
 class ApprovableCheckboxComponentPreview < ViewComponent::Preview
-  def default
+  # @!group States
+
+  def checked
+    approvable = FactoryBot.build_stubbed(:course)
+    student = UserStudent.new(FactoryBot.build_stubbed(:user, approvals: [approvable.id]))
+
+    render(ApprovableCheckboxComponent.new(approvable: approvable, subject_show: false, current_student: student))
+  end
+
+  def unchecked
     approvable = FactoryBot.build_stubbed(:course)
     student = UserStudent.new(FactoryBot.build_stubbed(:user))
 
     render(ApprovableCheckboxComponent.new(approvable: approvable, subject_show: false, current_student: student))
   end
+
+  def disabled
+    approvable = FactoryBot.build_stubbed(:course, :with_prerequisites)
+    student = UserStudent.new(FactoryBot.build_stubbed(:user))
+
+    render(ApprovableCheckboxComponent.new(approvable: approvable, subject_show: false, current_student: student))
+  end
+
+  # @!endgroup
 end
