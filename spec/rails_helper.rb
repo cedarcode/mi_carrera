ec506b2593f685d5fbb88a40d0f8d7a0eed357f0
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-04-28 19:28:43 -0300

    tests: add tests for `CheckboxInputComponent`

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require "view_component/test_helpers"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -73,6 +74,8 @@ RSpec.configure do |config|
       Rails.application.reload_routes_unless_loaded
     end
   end
+
+  config.include ViewComponent::TestHelpers, type: :component
 end
 
 Shoulda::Matchers.configure do |config|
