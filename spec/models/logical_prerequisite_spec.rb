0557fb134cd997f801c0f0dc1b86d902ade3915b
Braulio Martinez <braulio@cedarcode.com>
2025-04-30 20:29:43 -0300

    migrate logical_prerequisite test to rspec

--- spec/models/logical_prerequisite_spec.rb
+++ spec/models/logical_prerequisite_spec.rb
@@ -1,21 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe LogicalPrerequisite, type: :model do
-  describe 'associations' do
-    it {
-      should have_many(:operands_prerequisites)
-        .class_name('Prerequisite')
-        .with_foreign_key('parent_prerequisite_id')
-        .dependent(:destroy)
-        .inverse_of('parent_prerequisite')
-    }
+  let(:subject1) { create :subject }
+  let(:subject2) { create :subject }
+
+  describe '#met?' do
+    context 'on logical AND' do
+      it 'returns true when all prerequisites met' do
+        prerequisite = build(:and_prerequisite, operands_prerequisites: [
+          create(:subject_prerequisite, approvable_needed: subject1.course),
+          create(:subject_prerequisite, approvable_needed: subject2.course)
+        ])
+
+        expect(prerequisite.met?([subject1.course.id])).to be false
+        expect(prerequisite.met?([subject1.course.id, subject2.course.id])).to be true
+      end
+    end
+
+    context 'on logical OR' do
+      it 'returns true when any prerequisite met' do
+        prerequisite = build(:or_prerequisite, operands_prerequisites: [
+          create(:subject_prerequisite, approvable_needed: subject1.course),
+          create(:subject_prerequisite, approvable_needed: subject2.course)
+        ])
+
+        expect(prerequisite.met?([])).to be false
+        expect(prerequisite.met?([subject1.course.id])).to be true
+        expect(prerequisite.met?([subject2.course.id])).to be true
+      end
+    end
+
+    context 'on logical NOT' do
+      it 'returns true when prerequisite not met' do
+        prerequisite = build(:not_prerequisite, operands_prerequisites: [
+          create(:subject_prerequisite, approvable_needed: subject1.course)
+        ])
+
+        expect(prerequisite.met?([])).to be true
+        expect(prerequisite.met?([subject1.course.id])).to be false
+      end
+    end
+
+    context 'on logical AT_LEAST' do
+      it 'returns true when conditions are met' do
+        prerequisite = build(:at_least_prerequisite, amount_of_subjects_needed: 2, operands_prerequisites: [
+          create(:subject_prerequisite, approvable_needed: subject1.course),
+          create(:subject_prerequisite, approvable_needed: subject2.course),
+          create(:subject_prerequisite, approvable_needed: create(:subject).course),
+        ])
+
+        expect(prerequisite.met?([subject1.course.id])).to be false
+        expect(prerequisite.met?([subject1.course.id, subject2.course.id])).to be true
+      end
+    end
   end
 
   describe 'validations' do
-    it {
-      should validate_inclusion_of(:logical_operator)
-        .in_array(["and", "or", "not", "at_least"])
-        .with_message("#{Shoulda::Matchers::ExampleClass} is not a valid logical operator")
-    }
+    it 'validates that the amount of subjects is less than operand prerequisites count' do
+      prerequisite = build(:at_least_prerequisite, amount_of_subjects_needed: 3, operands_prerequisites: [
+        create(:subject_prerequisite, approvable_needed: subject1.course),
+        create(:subject_prerequisite, approvable_needed: subject2.course),
+      ])
+
+      expect(prerequisite).not_to be_valid
+    end
   end
 end
