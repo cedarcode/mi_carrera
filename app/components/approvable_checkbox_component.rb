7e2d9a1ff5bc1410059ab62285bce656388b11c2
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-04-30 18:39:21 -0300

    refactor: use `attr_reader` for accessing instance variables

--- app/components/approvable_checkbox_component.rb
+++ app/components/approvable_checkbox_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApprovableCheckboxComponent < ViewComponent::Base
+  attr_reader :approvable, :subject_show, :current_student
+
   def initialize(approvable:, subject_show:, current_student:)
     @approvable = approvable
     @subject_show = subject_show
