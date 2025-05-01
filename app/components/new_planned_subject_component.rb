378de1f85309b0b07132551edab65b704f032753
notable-sebastian <sebastian@notablefi.com>
2025-04-30 22:54:14 -0300

    add new planned subject component

---
+++ app/components/new_planned_subject_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class NewPlannedSubjectComponent < ViewComponent::Base
+  attr_reader :subjects
+
+  def initialize(subjects:)
+    @subjects = subjects
+  end
+
+  private
+
+  include SubjectsHelper
+end
