9c30773e2fb52e2ee3919370ea16e0665547f87a
PaoloCappelli <paolo.cappelli@betterup.co>
2025-05-01 01:36:20 -0300

    Remove null validation on rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,14 @@ class Review < ApplicationRecord
   belongs_to :subject
 
   validates :user_id, uniqueness: { scope: :subject_id, message: "You can only review a subject once." }
-  # validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :rating, inclusion: { in: 1..5 }, allow_nil: true
+  validate :at_least_one_review_field_present
+
+  private
+
+  def at_least_one_review_field_present
+    if rating.nil? && recommended.nil?
+      errors.add(:base, "At least one review field must be present")
+    end
+  end
 end
