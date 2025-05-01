f06e76a54351fb139301d0d4f78e70699b9d3099
PaoloCappelli <paolo.cappelli@betterup.co>
2025-05-01 02:10:00 -0300

    generalizing review destruction

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,15 @@ class Review < ApplicationRecord
   validates :rating, inclusion: { in: 1..5 }, allow_nil: true
   validate :at_least_one_review_field_present
 
+  # List of attributes that make up a review
+  REVIEW_FIELDS = [:rating, :recommended].freeze
+
+  # Returns true if all review fields are nil
+  def all_review_fields_blank?(except: [])
+    # undefined method 'except' for an instance of Array
+    REVIEW_FIELDS.reject { |field| except.include?(field) }.all? { |field| self.send(field).nil? }
+  end
+
   private
 
   def at_least_one_review_field_present
