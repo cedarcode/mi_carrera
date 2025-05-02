289016f34bbfe061bd3a328c60c4f7c6740c0da7
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-05-02 10:26:40 -0300

    refactor(`ApprovableCheckboxComponent`): use constant for checkbox classes

--- app/components/approvable_checkbox_component.rb
+++ app/components/approvable_checkbox_component.rb
@@ -1,6 +1,34 @@
 # frozen_string_literal: true
 
 class ApprovableCheckboxComponent < ViewComponent::Base
+  STYLES = {
+    checkbox: %w[
+      col-start-1
+      row-start-1
+      appearance-none
+      rounded-sm
+      border
+      border-gray-300
+      bg-white
+
+      checked:border-indigo-600
+      checked:bg-indigo-600
+
+      indeterminate:border-indigo-600
+      indeterminate:bg-indigo-600
+
+      focus-visible:outline-2
+      focus-visible:outline-offset-0
+      focus-visible:outline-indigo-600
+
+      disabled:border-gray-300
+      disabled:bg-gray-100
+      disabled:checked:bg-gray-100
+
+      forced-colors:appearance-auto
+    ],
+  }
+
   attr_reader :approvable, :subject_show, :current_student
 
   def initialize(approvable:, subject_show:, current_student:)
