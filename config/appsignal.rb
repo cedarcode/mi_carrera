851fdd1e69041173e4a64424c339daa7ee316bd1
Sebastian Suttner <sebastian@cedarcode.com>
2025-05-07 13:35:01 -0300

    chore(monitoring): add appsignal config

---
+++ config/appsignal.rb
@@ -0,0 +1,21 @@
+# AppSignal Ruby gem configuration
+# Visit our documentation for a list of all available configuration options.
+# https://docs.appsignal.com/ruby/configuration/options.html
+Appsignal.configure do |config|
+  config.activate_if_environment("development", "production")
+  config.name = "MiCarrera"
+
+  # The application's Push API key
+  # https://docs.appsignal.com/ruby/configuration/options.html#option-push_api_key
+  config.push_api_key = Rails.application.credentials.appsignal_push_api_key
+
+  # Configure actions that should not be monitored by AppSignal.
+  # For more information see our docs:
+  # https://docs.appsignal.com/ruby/configuration/ignore-actions.html
+  # config.ignore_actions << "ApplicationController#isup"
+
+  # Configure errors that should not be recorded by AppSignal.
+  # For more information see our docs:
+  # https://docs.appsignal.com/ruby/configuration/ignore-errors.html
+  # config.ignore_errors << "MyCustomError"
+end
