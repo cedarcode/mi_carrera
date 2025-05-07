5c9c77b7322c2ccdb817ef054862f94ebcf96b60
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-05-07 15:12:15 -0300

    Add `staging`, remove `development` from AppSignal

--- config/appsignal.rb
+++ config/appsignal.rb
@@ -2,7 +2,7 @@
 # Visit our documentation for a list of all available configuration options.
 # https://docs.appsignal.com/ruby/configuration/options.html
 Appsignal.configure do |config|
-  config.activate_if_environment("development", "production")
+  config.activate_if_environment("staging", "production")
   config.name = "MiCarrera"
 
   # The application's Push API key
