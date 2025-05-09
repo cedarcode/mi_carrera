5e4a20e20e0d97e82d6fa55fff2dd407aa16616c
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-05-09 20:50:53 -0300

    refactor: pass html options for input and container

--- app/components/text_field_component.rb
+++ app/components/text_field_component.rb
@@ -2,18 +2,15 @@ class TextFieldComponent < ViewComponent::Base
   renders_one :trailing_icon
   renders_one :hint
 
-  attr_reader :attribute, :form, :label, :required, :type
+  attr_reader :attribute, :container_options, :form, :input_options, :label, :type
 
-  def initialize(form:, attribute:, label:, type: :text, required: false)
+  def initialize(form:, attribute:, label:, type: :text, container_options: {}, input_options: {})
     @form = form
     @attribute = attribute
     @label = label
     @type = type.to_sym
-    @required = required
-  end
-
-  def password?
-    type == :password
+    @container_options = container_options
+    @input_options = input_options
   end
 
   def errors?
