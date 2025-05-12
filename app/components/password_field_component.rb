30d2adcbd1b73178f0ec0c67d1ccfa7f9dc9889d
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-05-12 16:10:09 -0300

    refactor: change `PasswordFieldComponent` approach

--- app/components/password_field_component.rb
+++ app/components/password_field_component.rb
@@ -1,32 +1,48 @@
 class PasswordFieldComponent < ViewComponent::Base
-  renders_one :hint
+  STYLES = {
+    input: %w[
+      bg-white
+      w-full
+      rounded-md
+      px-3
+      py-1.5
 
-  attr_reader :attribute, :form, :label, :container_options, :input_options
+      outline-1
+      outline-gray-300
 
-  def initialize(form:, attribute:, label:, container_options: {}, input_options: {})
+      focus:outline-2
+      focus:outline-indigo-600
+
+      with-error:outline-red-500
+      with-error:focus:outline-red-600
+    ],
+    icon: %w[
+      flex
+      items-center
+
+      absolute
+      inset-y-0
+      right-0
+      pr-3
+
+      cursor-pointer
+      hover:text-indigo-700
+    ]
+  }
+
+  attr_reader :form, :attribute, :label, :hint, :input_options
+
+  def initialize(form:, attribute:, label: nil, hint: nil, input_options: {})
     @form = form
     @attribute = attribute
-    @label = label
-    @container_options = container_options
-    @input_options = input_options
+    @label = label || attribute.to_s.humanize
+    @hint = hint
+    @input_options = input_options.merge(class: STYLES[:input], data: { show_password_target: "password" })
   end
 
-  def call
-    render TextFieldComponent.new(
-      form: form,
-      attribute: attribute,
-      label: label,
-      type: :password,
-      container_options: container_options.merge(data: { controller: "show-password" }),
-      input_options: input_options.merge(data: { show_password_target: "password" })
-    ) do |component|
-      component.with_hint do
-        hint.to_s if hint.present?
-      end
+  def error_messages
+    return unless form.object.errors[attribute].any?
 
-      component.with_trailing_icon do
-        render "shared/password_visibility_button"
-      end
-    end
+    form.object.errors[attribute].to_sentence.capitalize
   end
 end
