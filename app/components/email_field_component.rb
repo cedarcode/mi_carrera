37ee8d670ae874245e412a7bd15950bd260b03c4
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-05-12 15:38:13 -0300

    chore: add `EmailFieldComponent`

---
+++ app/components/email_field_component.rb
@@ -0,0 +1,35 @@
+class EmailFieldComponent < ViewComponent::Base
+  STYLES = {
+    input: %w[
+      bg-white
+      w-full
+      rounded-md
+      px-3
+      py-1.5
+
+      outline-1
+      outline-gray-300
+
+      focus:outline-2
+      focus:outline-indigo-600
+
+      with-error:outline-red-500
+      with-error:focus:outline-red-600
+    ]
+  }
+
+  attr_reader :attribute, :form, :label, :input_options
+
+  def initialize(form:, attribute:, label: nil, input_options: {})
+    @form = form
+    @attribute = attribute
+    @label = label
+    @input_options = input_options.merge(class: STYLES[:input])
+  end
+
+  def error_messages
+    return unless form.object.errors[attribute].any?
+
+    form.object.errors[attribute].to_sentence.capitalize
+  end
+end
