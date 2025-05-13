a364ab6d0714f5ec563436904e29cf56280018f3
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-05-12 22:01:28 -0300

    refactor(test): password helper

--- test/support/password_helpers.rb
+++ test/support/password_helpers.rb
@@ -1,12 +1,10 @@
 module PasswordHelpers
   def password_visibility_toggle_test(label_text)
-    container = find(:xpath, "//div[@data-controller='show-password'][.//label[text()='#{label_text}']]")
+    toggle = find_field(label_text).sibling("button[data-action='show-password#toggle']")
 
-    within(container) do
-      find("button[data-action='show-password#toggle']").click
-      assert_selector "input[type='text']"
-      find("button[data-action='show-password#toggle']").click
-      assert_selector "input[type='password']"
-    end
+    toggle.click
+    assert has_field?(label_text, type: 'text')
+    toggle.click
+    assert has_field?(label_text, type: 'password')
   end
 end
