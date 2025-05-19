a32ee03384fa26704396292058c4df1f541e88ce
rafaellamartino <rafaella.martino@cedarcode.com>
2025-05-19 12:00:48 -0300

    feat: adding `webauthn_id` to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_16_164747) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_19_145745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "unaccent"
@@ -103,6 +103,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_16_164747) do
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "locked_at"
     t.string "unlock_token"
+    t.string "webauthn_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
