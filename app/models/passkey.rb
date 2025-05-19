ab1db5ace8e5907be4dcdd9db288b7b3977be413
rafaellamartino <rafaella.martino@cedarcode.com>
2025-05-19 11:34:48 -0300

    feat: adding Passkey model verifications

--- app/models/passkey.rb
+++ app/models/passkey.rb
@@ -1,5 +1,10 @@
 class Passkey < ApplicationRecord
   belongs_to :user
+
+  validates :external_id, :public_key, :name, :sign_count, presence: true
+  validates :external_id, uniqueness: true
+  validates :sign_count,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: (2**32) - 1 }
 end
 
 # == Schema Information
