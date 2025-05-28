ce3e011d6f445581864c0a159f71b32fb9546b15
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-05-28 16:47:04 -0300

    Add Degree associations for Subjects and Groups

--- spec/factories/subject_groups.rb
+++ spec/factories/subject_groups.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :subject_group, aliases: [:group] do
     sequence(:code) { |n| "GROUP-#{n}" }
     sequence(:name) { |n| "Subject Group #{n}" }
+    degree
   end
 end
