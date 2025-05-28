b8d4dce2032bcae40e94afc2a37b992209024358
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-05-28 16:47:04 -0300

    Add Degree associations for Subjects and Groups

--- spec/models/degree_spec.rb
+++ spec/models/degree_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Degree, type: :model do
   describe 'associations' do
     it { should have_many(:users).dependent(:nullify) }
+    it { should have_many(:subjects).dependent(:destroy) }
+    it { should have_many(:subject_groups).dependent(:destroy) }
   end
 
   describe 'validations' do
