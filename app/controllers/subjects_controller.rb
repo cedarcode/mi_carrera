329a1186fbfaae099647d07d7cc047b954c2a686
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-05-28 17:25:00 -0300

    Scope queries to Subjects and Groups
    The queries are scoped to the student's degree

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
   def index
-    @subjects = TreePreloader.new(Subject.ordered_by_category_and_name).preload.select do |subject|
+    @subjects = TreePreloader.new(degree_subjects.ordered_by_category_and_name).preload.select do |subject|
       current_student.approved?(subject.course) ||
         (!subject.hidden_by_default? && current_student.available?(subject.course))
     end
@@ -17,12 +17,13 @@ class SubjectsController < ApplicationController
   def all
     subjects =
       if params[:search].present?
-        Subject
+        degree_subjects
           .where("lower(unaccent(name)) LIKE lower(unaccent(?))", "%#{params[:search].strip}%")
-          .or(Subject.where("lower(unaccent(short_name)) LIKE lower(unaccent(?))", "%#{params[:search].strip}%"))
-          .or(Subject.where("lower(code) LIKE lower(?)", "%#{params[:search].strip}%"))
+          .or(degree_subjects.where("lower(unaccent(short_name)) LIKE lower(unaccent(?))",
+                                    "%#{params[:search].strip}%"))
+          .or(degree_subjects.where("lower(code) LIKE lower(?)", "%#{params[:search].strip}%"))
       else
-        Subject
+        degree_subjects
       end.ordered_by_category_and_name
 
     @subjects = TreePreloader.new(subjects).preload
@@ -31,6 +32,10 @@ class SubjectsController < ApplicationController
   private
 
   def subject
-    @subject ||= Subject.find(params[:id])
+    @subject ||= degree_subjects.find(params[:id])
+  end
+
+  def degree_subjects
+    current_student.degree&.subjects || Subject
   end
 end
