14e3692f62b75c786518dd536cf9fd2a3e9c2361
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-05-28 13:27:59 -0300

    Add tests for Degree model

---
+++ spec/models/degree_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Degree, type: :model do
+  describe 'validations' do
+    subject { create :degree }
+
+    it { is_expected.to validate_presence_of(:key) }
+    it { is_expected.to validate_uniqueness_of(:key) }
+  end
+end
