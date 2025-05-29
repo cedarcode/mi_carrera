9a45abccf2ce8929645816e6b6b37950ad5e03df
PaoloCappelli <paolo.cappelli@betterup.co>
2025-05-28 23:41:28 -0300

    Refactor ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,15 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_review, only: [:destroy]
 
   def create
-    @review = current_user.reviews.find_or_initialize_by(subject_id: params[:subject_id])
-    @review.update!(rating: params[:rating])
-
-    redirect_to subject_path(@review.subject)
-  end
-
-  def destroy
-    @review.destroy!
-
-    redirect_to subject_path(@review.subject)
+    review = current_user.reviews.find_or_initialize_by(subject_id: params[:subject_id])
+    review.update!(permitted_params)
+    redirect_to subject_path(review.subject)
   end
 
   private
 
-  def set_review
-    @review = current_user.reviews.find(params[:id])
+  def permitted_params
+    params.permit(:recommended, :interesting, :credits_to_difficulty_ratio)
   end
 end
