c25ca6618c47e304dfdf15c014d8cd3a9524809c
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-05-28 21:20:17 -0300

    chore: add button component

---
+++ app/components/button_component.rb
@@ -0,0 +1,14 @@
+class ButtonComponent < ViewComponent::Base
+  attr_reader :form, :label
+
+  def initialize(form:, label:)
+    @form = form
+    @label = label
+  end
+
+  def call
+    klass = 'bg-primary text-white font-bold shadow-sm cursor-pointer p-2 rounded-md hover:shadow-md'
+
+    @form.button @label, class: klass
+  end
+end
