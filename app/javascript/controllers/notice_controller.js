158cf3eb19ee61c8a12fe702e7ac42925f5377a3
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-05-28 22:53:06 -0300

    fix: enter animation not working

--- app/javascript/controllers/notice_controller.js
+++ app/javascript/controllers/notice_controller.js
@@ -4,11 +4,12 @@ export default class extends Controller {
   connect() {
     const toast = this.element
 
-    toast.classList.remove("hidden")
     toast.classList.remove("translate-x-full", "opacity-0")
+    toast.classList.add("mr-4")
 
     setTimeout(() => {
-      toast.classList.add("translate-x-full", "opacity-0")
+      toast.classList.add("translate-x-full", "opacity-0", "mr-4")
+      toast.classList.remove("mr-4")
 
       toast.addEventListener("transitionend", () => {
         toast.classList.add("hidden")
