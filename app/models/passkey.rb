b62a4cd46fdc38eba8874d93135892d91a46e1fb
rafaellamartino <rafaella.martino@cedarcode.com>
2025-06-02 10:06:23 -0300

    feat: simplifying passkey validations

--- app/models/passkey.rb
+++ app/models/passkey.rb
@@ -3,8 +3,7 @@ class Passkey < ApplicationRecord
 
   validates :external_id, :public_key, :name, :sign_count, presence: true
   validates :external_id, uniqueness: true
-  validates :sign_count,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: (2**32) - 1 }
+  validates :sign_count, numericality: { only_integer: true, in: 0..2**32 - 1 }
 end
 
 # == Schema Information
