10255195bac8b45985cbdcf719b234d31034f7e2
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-03 15:59:52 -0300

    chore: implement logic for removing incorrect exams

--- script/remove_incorrect_exams.rb
+++ script/remove_incorrect_exams.rb
@@ -1,3 +1,13 @@
 require_relative "../config/environment"
 
-# Your code goes here
+subjects_data = YAML.load_file(Rails.root.join("db/data/computacion/scraped_subjects.yml"))
+Subject.with_exam.select { |subject| !subjects_data[subject.code]["has_exam"] }.each do |subject|
+  destroyed_approvable = subject.exam.destroy!
+
+  User
+    .select { |user| user.approvals.include?(destroyed_approvable.id) }
+    .each do |user|
+      user.approvals.delete(destroyed_approvable.id)
+      user.save!
+    end
+end
