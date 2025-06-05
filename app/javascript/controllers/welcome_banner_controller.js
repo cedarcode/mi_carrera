9da5bf370cc2d39b8ff78f3b38bcc9422f4aa025
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-05 19:02:14 -0300

    feat: prevent banner for hiding content
    The previous banner from MaterialUI that we were using was able to
    render without hiding the rest of the content of the page.
    
    This commits makes the new banner behave in a similar way, following a
    similar approach than the one using before: using a relative div with
    the same heigh than the banner for reserving the space that the banner
    would take. This is in turn achieved by using Javascript to calculate
    the height of the banner and setting the height of the relative div to
    be the same.

--- app/javascript/controllers/welcome_banner_controller.js
+++ app/javascript/controllers/welcome_banner_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus";
 import { MDCBanner } from "@material/banner";
 
 export default class extends Controller {
+  static targets = ["banner"];
+
+  connect() {
+    let contentHeight = this.bannerTarget.offsetHeight;
+    this.element.style.setProperty("height", `${contentHeight}px`);
+  }
+
   dismiss() {
     this.element.remove();
   }
