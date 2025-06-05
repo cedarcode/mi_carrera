71953803e88cdb447998cb1a52a9c4596ecfcd5c
Paula Mazzeo <paula.mazzeo@betterup.co>
2025-06-05 16:27:41 -0300

    Simplify user menu controller

--- app/javascript/controllers/user_menu_controller.js
+++ app/javascript/controllers/user_menu_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["menu", "trigger"];
-  static hiddenClass = "hidden";
 
   connect() {
     // If the JS takes some time to load, it is possible to click on the
@@ -14,28 +13,16 @@ export default class extends Controller {
     // attribute to the element which we will use in the CSS matcher for
     // finding the button – that way capybara will only click on it when the
     // controller it's connected.
-    this.triggerTarget.setAttribute('data-controller-connected', 'true');
+    this.triggerTarget.setAttribute("data-controller-connected", "true");
   }
 
   toggle() {
-    if (this.menuTarget.classList.contains(this.constructor.hiddenClass)) {
-      this.open();
-    } else {
-      this.close();
-    }
-  }
-
-  open() {
-    this.menuTarget.classList.remove(this.constructor.hiddenClass);
-  }
-
-  close() {
-    this.menuTarget.classList.add(this.constructor.hiddenClass);
+    this.menuTarget.classList.toggle("hidden");
   }
 
   onClick() {
-    if (!this.menuTarget.classList.contains(this.constructor.hiddenClass)) {
-      this.close();
+    if (!this.menuTarget.classList.contains("hidden")) {
+      this.toggle();
     }
   }
 }
