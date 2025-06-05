bcf9224d8486d28d11cca0659b060a717a1cc52e
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-05 18:53:03 -0300

    feature: migrate banner to tailwind

--- app/javascript/controllers/welcome_banner_controller.js
+++ app/javascript/controllers/welcome_banner_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 import { MDCBanner } from "@material/banner";
 
 export default class extends Controller {
-  connect() {
-    new MDCBanner(this.element).open();
+  dismiss() {
+    this.element.remove();
   }
 }
