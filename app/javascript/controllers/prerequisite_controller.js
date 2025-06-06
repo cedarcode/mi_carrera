cb4cd22f717588fe7442f5cafab778448dba41ba
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-06-06 12:34:50 -0300

    feat: migrate `prerequisite` view to use `heroicons`

--- app/javascript/controllers/prerequisite_controller.js
+++ app/javascript/controllers/prerequisite_controller.js
@@ -1,15 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "collapsable", "button" ]
+  static targets = [ "collapsable", "chevronRight", "chevronDown" ]
 
   toggle() {
     if (this.collapsableTarget.style.display == "block") {
       this.collapsableTarget.style.display = "none";
-      this.buttonTarget.innerHTML = "chevron_right"
+      this.chevronRightTarget.classList.toggle("hidden");
+      this.chevronDownTarget.classList.toggle("hidden");
     } else {
       this.collapsableTarget.style.display = "block";
-      this.buttonTarget.innerHTML = "expand_more";
+      this.chevronRightTarget.classList.toggle("hidden");
+      this.chevronDownTarget.classList.toggle("hidden");
     }
   }
 }
