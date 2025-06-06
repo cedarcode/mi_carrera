76cd18c14c38d7f5ff368c2332dd2880f43080c0
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-06 16:58:46 -0300

    feat: allow navbar to withstand window being resized

--- app/javascript/controllers/welcome_banner_controller.js
+++ app/javascript/controllers/welcome_banner_controller.js
@@ -1,13 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["content"];
-
-  connect() {
-    let contentHeight = this.contentTarget.offsetHeight;
-    this.element.style.setProperty("height", `${contentHeight}px`);
-  }
-
   dismiss() {
     this.element.remove();
   }
