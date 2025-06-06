c6593dba5971cd3cd7ed9bda6a938940ee096f77
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-06-06 14:13:26 -0300

    feat: migrate `eye` icon

--- app/javascript/controllers/show_password_controller.js
+++ app/javascript/controllers/show_password_controller.js
@@ -1,15 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["password", "icon"];
+  static targets = ["password", "eye", "eyeSlash"];
 
   toggle() {
     if (this.passwordTarget.type === "password") {
       this.passwordTarget.type = "text";
-      this.iconTarget.innerHTML = "visibility_off";
+      this.eyeTarget.classList.toggle("hidden");
+      this.eyeSlashTarget.classList.toggle("hidden");
     } else {
       this.passwordTarget.type = "password";
-      this.iconTarget.innerHTML = "visibility";
+      this.eyeTarget.classList.toggle("hidden");
+      this.eyeSlashTarget.classList.toggle("hidden");
     }
   }
 }
