486be2c0c156c8b7d9dc5f8d004876f8697b3ecd
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-06-11 14:08:18 -0300

    chore: remove `rails/ujs` code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
-import Rails from "@rails/ujs";
 import "install";
 import "serviceworker-companion";
-
-Rails.start();
 
 Turbo.session.drive = false
