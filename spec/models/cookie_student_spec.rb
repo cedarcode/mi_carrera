bd5798deab94140a700aa53cf06fd431f2b0ded7
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-06-12 15:42:49 -0300

    Store `computacion` in cookie if it's not set

--- spec/models/cookie_student_spec.rb
+++ spec/models/cookie_student_spec.rb
@@ -221,6 +221,38 @@ RSpec.describe CookieStudent, type: :model do
     end
   end
 
+  describe '#degree' do
+    let(:student) { build(:cookie_student, cookies:) }
+
+    context 'when degree_name is not set in cookie' do
+      let!(:degree) { create :degree, name: "computacion" }
+      let(:cookies) { build(:cookie) }
+
+      it 'returns default degree' do
+        expect(student.degree).to eq(Degree.default)
+      end
+
+      it 'stores default degree in cookie' do
+        student.degree
+        expect(cookies[:degree_name]).to eq(Degree.default.name)
+      end
+    end
+
+    context 'when degree_name is set in cookie' do
+      let!(:degree) { create :degree, name: "not_computacion" }
+      let(:cookies) { build(:cookie, degree_name: 'not_computacion') }
+
+      it 'returns the degree stored in the cookie' do
+        expect(student.degree).to eq(degree)
+      end
+
+      it 'does not store default degree in cookie' do
+        student.degree
+        expect(cookies[:degree_name]).to eq(degree.name)
+      end
+    end
+  end
+
   describe '#degree_subjects' do
     let!(:degree) { create :degree, name: "computacion" }
     let(:student) { build :cookie_student }
