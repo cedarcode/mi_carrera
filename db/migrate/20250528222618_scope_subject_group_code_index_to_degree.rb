63e6627cc8a88c1fb3a87c5d7ca1c324f9b5b887
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-06-13 12:55:46 -0300

    Add index before removing the previous one

--- db/migrate/20250528222618_scope_subject_group_code_index_to_degree.rb
+++ db/migrate/20250528222618_scope_subject_group_code_index_to_degree.rb
@@ -1,6 +1,6 @@
 class ScopeSubjectGroupCodeIndexToDegree < ActiveRecord::Migration[8.0]
   def change
-    remove_index :subject_groups, :code
     add_index :subject_groups, [:degree_id, :code], unique: true
+    remove_index :subject_groups, :code
   end
 end
--- db/migrate/20250528222931_scope_subject_code_index_to_degree.rb
+++ db/migrate/20250528222931_scope_subject_code_index_to_degree.rb
@@ -1,6 +1,6 @@
 class ScopeSubjectCodeIndexToDegree < ActiveRecord::Migration[8.0]
   def change
-    remove_index :subjects, :code
     add_index :subjects, [:degree_id, :code], unique: true
+    remove_index :subjects, :code
   end
 end
