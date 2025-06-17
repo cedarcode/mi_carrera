a267bd36181a1b2735987a027d3569def44cdf51
rafaellamartino <rafaella.martino@cedarcode.com>
2025-06-17 12:26:46 -0300

    feat: add sessions controller and view to handle sign in through passkey

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,12 @@
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
+
+    def new
+      super do
+        @get_passkey_options = WebAuthn::Credential.options_for_get(user_verification: 'required')
+        session[:current_authentication] = { challenge: @get_passkey_options.challenge }
+      end
+    end
+  end
+end
