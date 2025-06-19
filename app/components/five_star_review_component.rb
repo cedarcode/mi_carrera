afdfd65ca1df3946267ccce2a51fe5788adb9776
PaoloCappelli <paolo.cappelli@betterup.co>
2025-06-18 22:55:52 -0300

    Use button_to instead of form_with

--- app/components/five_star_review_component.rb
+++ app/components/five_star_review_component.rb
@@ -1,4 +1,15 @@
 class FiveStarReviewComponent < ViewComponent::Base
+  STYLES = {
+    form: %w[
+      transition-transform
+      duration-[250ms]
+      ease-[ease]
+      hover:scale-[1.3]
+      [&:hover~&]:scale-[1.3]
+    ],
+    button: %w[material-icons cursor-pointer !text-xl]
+  }
+
   def initialize(review_name:, rating_value: nil, subject_id:, user_review: nil, column_name:)
     @review_name = review_name
     @rating_value = rating_value
@@ -10,23 +21,31 @@ class FiveStarReviewComponent < ViewComponent::Base
 
   private
 
-  attr_reader :review_name, :rating_value, :subject_id, :column_name
+  attr_reader :review_name
 
   def display_rating
-    number_with_precision(rating_value, precision: 1) || '-.-'
+    number_with_precision(@rating_value, precision: 1) || '-.-'
   end
 
-  def star_options
-    5.downto(1).map do |value|
-      {
-        value:,
-        url: selected?(value) ? review_path(@user_review) : reviews_path,
-        form_method: selected?(value) ? :delete : :post,
-        button_icon: filled?(value) ? 'star' : 'star_outline',
-        button_classes: filled?(value) ? 'text-violet-400' : 'text-gray-400'
-      }
-    end
+  def star_button(value)
+    button_to star_icon(value), star_url(value),
+              method: star_form_method(value),
+              params: star_params(value),
+              class: star_button_classes(value),
+              form: { class: STYLES[:form] }
   end
+
+  def star_icon(value) = filled?(value) ? 'star' : 'star_outline'
+
+  def star_url(value) = selected?(value) ? review_path(@user_review) : reviews_path
+
+  def star_form_method(value) = selected?(value) ? :delete : :post
+
+  def star_params(value) = { subject_id: @subject_id, @column_name => value }
+
+  def star_button_classes(value) = STYLES[:button] + [star_color_classes(value)]
+
+  def star_color_classes(value) = filled?(value) ? 'text-violet-400' : 'text-gray-400'
 
   def filled?(value) = @user_review_rating&.>= value
 
