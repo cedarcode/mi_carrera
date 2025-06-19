6eacb6a7cdd9088d75764432428ba07690117046
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-19 00:43:42 -0300

    feat: allow dragging not approved subjects

--- app/javascript/controllers/draggable_controller.js
+++ app/javascript/controllers/draggable_controller.js
@@ -1,26 +1,47 @@
 import { Controller } from "@hotwired/stimulus"
 import Sortable from 'sortablejs';
-import { put } from '@rails/request.js';
+import { put, post } from '@rails/request.js';
 
 export default class extends Controller {
   connect() {
     var lists = document.querySelectorAll('ul');
+    var notApprovedSubjectsList = document.querySelector('ul#not-approved-subjects');
 
     lists.forEach((list) => {
+      if (list.id === 'not-approved-subjects') return;
+
       Sortable.create(list, {
         group: 'shared',
         sort: false,
         onEnd: this.onEnd.bind(this),
       });
     });
+
+    notApprovedSubjectsList && Sortable.create(notApprovedSubjectsList, {
+      group: {
+        name: 'shared',
+        put: false
+      },
+      sort: false,
+      onEnd: this.onEnd.bind(this),
+    });
   }
 
   onEnd(event) {
     const url = event.item.dataset.draggableUrl;
+    const method = event.item.dataset.draggableMethod;
     const newSemester = event.to.parentElement.dataset.semester;
 
-    put(url, {
-      body: JSON.stringify({ semester: newSemester })
-    });
+    if (method == 'put') {
+      put(url, {
+        body: JSON.stringify({ semester: newSemester })
+      });
+    } else if (method == 'post') {
+      const subjectId = event.item.dataset.draggableSubjectId;
+
+      post(url, {
+        body: JSON.stringify({ subject_plan: { subject_id: subjectId, semester: newSemester } }),
+      });
+    }
   }
 }
