65a60fa5a875b9aaab2e99dd82e9a5c0d2b095e1
PaoloCappelli <paolo.cappelli@betterup.co>
2025-06-18 23:56:02 -0300

    Update rating attribute in 5 star component preview

--- spec/components/previews/five_star_review_component_preview.rb
+++ spec/components/previews/five_star_review_component_preview.rb
@@ -5,19 +5,19 @@ class FiveStarReviewComponentPreview < ViewComponent::Preview
     render(FiveStarReviewComponent.new(
              review_name: "Puntuación",
              subject_id: 123,
-             rating_attribute: :rating
+             rating_attribute: :interesting_rating
            ))
   end
 
   def with_user_review
-    user_review = FactoryBot.build_stubbed(:review)
+    user_review = FactoryBot.build_stubbed(:review, interesting_rating: 3)
 
     render(FiveStarReviewComponent.new(
              review_name: "Puntuación",
              rating_value: 3.8,
              subject_id: user_review.subject_id,
              user_review: user_review,
-             rating_attribute: :rating,
+             rating_attribute: :interesting_rating,
            ))
   end
   # @!endgroup
