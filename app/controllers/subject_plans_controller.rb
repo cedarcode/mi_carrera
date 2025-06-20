59b33f5306c788459f82075b858c82205c8d2727
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-20 16:20:18 -0300

    feat(planner): avoid reloading the page when removing planned subject

--- app/controllers/subject_plans_controller.rb
+++ app/controllers/subject_plans_controller.rb
@@ -12,13 +12,18 @@ class SubjectPlansController < ApplicationController
     @semester = subject_plan_params[:semester].to_i
 
     set_planned_and_not_planned_subjects
+
+    render :update
   end
 
   def destroy
     subject_plan = current_user.subject_plans.find_by!(subject_id: params[:subject_id])
+    @semester = subject_plan.semester
     subject_plan.destroy!
 
-    redirect_to subject_plans_path
+    set_planned_and_not_planned_subjects
+
+    render :update
   end
 
   private
