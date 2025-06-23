c900459eda29d270090c0be919bcb01d878e0259
joaquintomas2003 <joaquin.tomas@cedarcode.com>
2025-06-23 11:37:07 -0300

    Scope queries to current degree

--- app/controllers/subject_groups_controller.rb
+++ app/controllers/subject_groups_controller.rb
@@ -1,12 +1,8 @@
 class SubjectGroupsController < ApplicationController
   def show
-    @subject_group = degree_subject_groups.find(params[:id])
+    @subject_group = current_degree.subject_groups.find(params[:id])
     respond_to do |format|
       format.html
     end
-  end
-
-  def degree_subject_groups
-    current_degree.subject_groups
   end
 end
