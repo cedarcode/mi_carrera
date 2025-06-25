8affbcdc104c2f9fff339409ae754d35da7b3749
rafaellamartino <rafaella.martino@cedarcode.com>
2025-06-25 11:21:42 -0300

    feat: adding rollback to migration changing webauthn_id to uuid

--- db/migrate/20250624185553_change_webauthn_id_to_uuid_in_users.rb
+++ db/migrate/20250624185553_change_webauthn_id_to_uuid_in_users.rb
@@ -7,5 +7,9 @@ class ChangeWebauthnIdToUuidInUsers < ActiveRecord::Migration[8.0]
   end
 
   def down
+    change_table :users, bulk: true do |t|
+      t.remove :webauthn_id
+      t.column :webauthn_id, :string
+    end
   end
 end
