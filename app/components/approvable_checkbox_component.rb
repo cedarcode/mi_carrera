816240da601428c574f81d90885ee475a064254a
Renzo Minelli <renminelli@gmail.com>
2025-06-27 22:03:15 -0300

    agregar filtros en all e index

--- app/components/approvable_checkbox_component.rb
+++ app/components/approvable_checkbox_component.rb
@@ -38,15 +38,16 @@ class ApprovableCheckboxComponent < ViewComponent::Base
     ],
   }
 
-  attr_reader :approvable, :subject_show, :current_student
-
-  def initialize(approvable:, subject_show:, current_student:)
+  def initialize(approvable:, subject_show:, current_student:, filter_categories: Subject::MAIN_CATEGORIES)
     @approvable = approvable
     @subject_show = subject_show
     @current_student = current_student
+    @filter_categories = filter_categories
   end
 
   private
+
+  attr_reader :approvable, :subject_show, :current_student, :filter_categories
 
   def checked? = current_student.approved?(approvable)
 
