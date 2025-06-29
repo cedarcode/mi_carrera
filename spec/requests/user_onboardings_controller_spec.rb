4b46b17380db8313a2a052ca918594a2473d62c9
santiagoguridi <santiago.guridi@cedarcode.com>
2025-06-28 22:06:36 -0300

    fix and improve specs

--- spec/requests/user_onboardings_controller_spec.rb
+++ spec/requests/user_onboardings_controller_spec.rb
@@ -3,20 +3,62 @@ require 'rails_helper'
 RSpec.describe UserOnboardingsController, type: :request do
   describe 'PATCH #update' do
     context 'when signed in' do
-      it 'returns http ok' do
-        sign_in create(:user)
+      let(:user) { create(:user) }
 
-        patch user_onboardings_path, params: { format: :json }
+      before { sign_in user }
 
-        expect(response).to have_http_status(:ok)
+      context 'with valid banner type "welcome"' do
+        it 'marks welcome banner as viewed and returns ok' do
+          patch user_onboardings_path, params: { banner_type: 'welcome', format: :json }
+
+          expect(response).to have_http_status(:ok)
+          expect(user.reload.welcome_banner_viewed).to be true
+        end
+      end
+
+      context 'with valid banner type "planner"' do
+        it 'marks planner banner as viewed and returns ok' do
+          patch user_onboardings_path, params: { banner_type: 'planner', format: :json }
+
+          expect(response).to have_http_status(:ok)
+          expect(user.reload.planner_banner_viewed).to be true
+        end
+      end
+
+      context 'with invalid banner type' do
+        it 'returns bad request' do
+          patch user_onboardings_path, params: { banner_type: 'invalid', format: :json }
+
+          expect(response).to have_http_status(:bad_request)
+          expect(JSON.parse(response.body)).to eq({ 'error' => 'Invalid banner type' })
+        end
       end
     end
 
     context 'when not signed in' do
-      it 'returns http ok' do
-        patch user_onboardings_path, params: { format: :json }
+      context 'with valid banner type "welcome"' do
+        it 'updates cookie and returns ok' do
+          patch user_onboardings_path, params: { banner_type: 'welcome', format: :json }
 
-        expect(response).to have_http_status(:ok)
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'with valid banner type "planner"' do
+        it 'returns ok (planner banner not supported for anonymous users)' do
+          patch user_onboardings_path, params: { banner_type: 'planner', format: :json }
+
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'with invalid banner type' do
+        it 'returns bad request' do
+          patch user_onboardings_path, params: { banner_type: 'invalid', format: :json }
+
+          expect(response).to have_http_status(:bad_request)
+          expect(JSON.parse(response.body)).to eq({ 'error' => 'Invalid banner type' })
+        end
       end
     end
   end
