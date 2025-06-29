c7f8fe263e6cc46b527b88e0c2794535c00f4b7a
santiagoguridi <santiago.guridi@cedarcode.com>
2025-06-28 21:22:55 -0300

    generalize welcome banner

--- app/controllers/user_onboardings_controller.rb
+++ app/controllers/user_onboardings_controller.rb
@@ -1,6 +1,16 @@
 class UserOnboardingsController < ApplicationController
   def update
-    current_student.welcome_banner_mark_as_viewed!
+    banner_type = params[:banner_type]
+
+    case banner_type
+    when 'planner'
+      current_student.planner_banner_mark_as_viewed!
+    when 'welcome'
+      current_student.welcome_banner_mark_as_viewed!
+    else
+      render json: { error: 'Invalid banner type' }, status: :bad_request
+      return
+    end
 
     head :ok
   end
