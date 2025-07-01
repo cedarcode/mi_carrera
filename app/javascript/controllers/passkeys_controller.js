e42f52c2308bc265f6a6aebb099f34b0c9b48b7d
rafaellamartino <rafaella.martino@cedarcode.com>
2025-07-01 14:39:22 -0300

    feat: let devise handle redirect if strategy succeed

--- app/javascript/controllers/passkeys_controller.js
+++ app/javascript/controllers/passkeys_controller.js
@@ -28,23 +28,18 @@ export default class extends Controller {
     try {
       const passkeyOptions = WebAuthnJSON.parseRequestOptionsFromJSON({ publicKey });
       const passkeyPublicKey = await WebAuthnJSON.get(passkeyOptions);
-      const formData = new FormData(this.element);
-      formData.append('passkey_public_key', JSON.stringify(passkeyPublicKey));
 
-      const response = await fetch(this.element.action, {
-        method: this.element.method,
-        body: formData,
-        headers: {
-          "accept": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        window.location.href = response.headers.get("Location") || '/';
-      }  else {
-        const json = await response.json();
-        alert(json.error || "Ocurrió un error al intentar ingresar con su llave de seguridad.");
+      let hiddenPasskeyInput = this.element.querySelector("input[name='passkey_public_key']");
+      if (!hiddenPasskeyInput) {
+        hiddenPasskeyInput = document.createElement("input");
+        hiddenPasskeyInput.type = "hidden";
+        hiddenPasskeyInput.name = "passkey_public_key";
+        this.element.appendChild(hiddenPasskeyInput);
       }
+      hiddenPasskeyInput.value = JSON.stringify(passkeyPublicKey);
+
+      this.element.submit();
+
     } catch (error) {
       alert(error);
     }
