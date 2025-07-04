4f48d2ec02f4e497bc1e4436045153f63227b6a6
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-07-04 19:40:18 -0300

    feat(planner): remove unnecessary label from add subject section

--- app/javascript/controllers/subject_selector_controller.js
+++ app/javascript/controllers/subject_selector_controller.js
@@ -8,13 +8,13 @@ export default class extends Controller {
     this.choices = new Choices(this.selectTarget, {
       searchEnabled: true,
       searchPlaceholderValue: "Buscar materia...",
-      placeholderValue: "Seleccionar materia...",
+      placeholderValue: "Seleccionar materia para planificar...",
       itemSelectText: "",
       noResultsText: "No se encontraron materias",
       noChoicesText: "No hay materias disponibles",
       shouldSort: false,
       classNames: {
-        containerOuter: ["choices", "!m-0", "!h-10"],
+        containerOuter: ["choices", "!m-0", "!h-10", "flex-grow-1"],
         containerInner: [
           "choices__inner",
           "!min-h-10",
