2ae23e453aa83f1a9e1466d5fcf55f0521949260
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2025-07-20 11:44:16 -0300

    Use display_name in subject dropdown for consistency

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -37,10 +37,6 @@ module SubjectsHelper
     "#{subject.code} - #{subject.short_name || subject.name}"
   end
 
-  def display_planned_subject_name_and_code(subject)
-    "#{subject.short_name || subject.name} - #{subject.code}"
-  end
-
   def display_subject_prerequisite(subject_prerequisite)
     approvable = subject_prerequisite.approvable_needed
     "#{display_name(approvable.subject)} (#{approvable.is_exam ? "examen" : "curso"})"
