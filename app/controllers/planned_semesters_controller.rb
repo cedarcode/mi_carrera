671cbae5b713cae39cf8cec2b366fdca07113582
Paula Mazzeo <paula.mazzeo@betterup.co>
2025-07-24 22:00:03 -0300

    Address cr

--- app/controllers/planned_semesters_controller.rb
+++ app/controllers/planned_semesters_controller.rb
@@ -4,11 +4,6 @@ class PlannedSemestersController < ApplicationController
   def create
     current_user.update!(planned_semesters: current_user.planned_semesters + 1)
 
-    @not_planned_subjects =
-      TreePreloader.preload(Subject.ordered_by_category.ordered_by_short_or_full_name).reject do |subject|
-        current_user.planned?(subject) || current_student.approved?(subject)
-      end
-
     @semester = current_user.planned_semesters
   end
 end
