66419993412472f63448195652872f4e13e9245e
rafaella-martino <rafaella.martino@cedarcode.com>
2025-07-25 15:56:08 -0300

    last test

--- app/services/strategies/passkey_strategy.rb
+++ app/services/strategies/passkey_strategy.rb
@@ -19,12 +19,24 @@ module Strategies
           sign_count: passkey.sign_count,
           user_verification: true
         )
+        if webauthn_passkey.error
+          self.fail("Passkey verification failed and an error was kept: #{webauthn_passkey.error}")
+          return
+        end
+
+        if webauthn_passkey.sign_count == nil
+          self.fail("Passkey sign count is missing")
+          return
+        end
 
         if passkey.update!(sign_count: webauthn_passkey.sign_count)
           self.fail("Passkey verification failed")
+          return
         end
 
         success!(passkey.user)
+      rescue WebAuthn::Error => e
+        self.fail("Passkey verification error and it raised exception: #{e.message}")
       ensure
         session.delete(:creation_challenge)
       end
