87f6df3bb137d3339ed981b7c98a4cf69bfc24ac
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-07-28 19:07:56 -0300

    chore: remove unnecessary event prevent default

--- app/javascript/controllers/planner_loading_controller.js
+++ app/javascript/controllers/planner_loading_controller.js
@@ -5,9 +5,6 @@ export default class extends Controller {
     var foregroundLayer = document.createElement("div");
     foregroundLayer.classList.add("bg-gray-300", "min-h-full", "w-full", "absolute", "top-0", "opacity-40");
     foregroundLayer.setAttribute("id", "foreground-layer");;
-    foregroundLayer.addEventListener("click", function(event) {
-      event.preventDefault();
-    });
     this.element.append(foregroundLayer);
   }
 }
