c25d6ee7787d5dc572329c02aa4ab1d267e9423c
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-07-28 19:16:41 -0300

    refactor: always render the foreground layer with `hidden` class

--- app/javascript/controllers/planner_loading_controller.js
+++ app/javascript/controllers/planner_loading_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["foregroundLayer"]
+
   start() {
-    var foregroundLayer = document.createElement("div");
-    foregroundLayer.classList.add("bg-gray-300", "min-h-full", "w-full", "absolute", "top-0", "opacity-40");
-    foregroundLayer.setAttribute("id", "foreground-layer");;
-    this.element.append(foregroundLayer);
+    this.foregroundLayerTarget.classList.remove("hidden");
   }
 }
