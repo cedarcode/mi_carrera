2716ac13fca01fdc1d49a9990f809e2442acea6e
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-12-01 12:23:57 -0300

    chore: update `bin/ci`

--- config/ci.rb
+++ config/ci.rb
@@ -5,12 +5,10 @@ CI.run do
 
   step "Style: Ruby", "bin/rubocop"
 
-  step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
-  step "Tests: Rails", "bin/rails test"
-  step "Tests: System", "bin/rails test:system"
+  step "Tests: Rails", "bin/bundle exec rspec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
