3a17baff0d88830bda65bd19c4edcf094e2f9668
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-12-09 12:59:44 -0300

    chore: update views to use `Devise::Webauthn` helpers
    These helpers work straight up with the webauthn controller generated by
    `Devise::Webauthn`

--- app/controllers/users/passkeys_controller.rb
+++ app/controllers/users/passkeys_controller.rb
@@ -2,20 +2,7 @@ module Users
   class PasskeysController < Devise::PasskeysController
     prepend_before_action :ensure_feature_enabled!
 
-    def index
-      @create_passkey_options = WebAuthn::Credential.options_for_create(
-        user: {
-          id: current_user.webauthn_id,
-          name: current_user.email
-        },
-        exclude: current_user.passkeys.pluck(:external_id),
-        authenticator_selection: {
-          resident_key: 'required',
-          user_verification: 'required'
-        }
-      )
-      session[:creation_challenge] = @create_passkey_options.challenge
-    end
+    def index; end
 
     private
 
