48072bd4b1353c892b7bb80b288e4cc80e89f61c
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-12-09 11:43:32 -0300

    chore: clean up passkeys logic

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,7 +9,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "canvas-confetti", to: "https://ga.jspm.io/npm:canvas-confetti@1.6.0/dist/confetti.module.mjs"
 pin "choices.js", to: "https://ga.jspm.io/npm:choices.js@11.1.0/public/assets/scripts/choices.js"
-pin "@github/webauthn-json/browser-ponyfill", to: "@github--webauthn-json--browser-ponyfill.js"
 
 pin "sortablejs" # @1.15.6
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.12
