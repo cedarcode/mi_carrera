d2051e1c5ef440b7ec81becc7b23ec3b642c003c
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2026-01-23 15:00:17 -0300

    refactor: change name of stimulus controller to `webauthn-support`

--- app/javascript/controllers/webauthn_support_controller.js
+++ app/javascript/controllers/webauthn_support_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["createPasskeyButton", "unsupportedMessage"];
+  static targets = ["button", "unsupportedMessage"];
 
   hide() {
     this.element.classList.add("hidden");
   }
 
-  disable() {
-    this.createPasskeyButtonTarget.disabled = true;
+  disableButton() {
+    this.buttonTarget.disabled = true;
     this.unsupportedMessageTarget.hidden = false;
   }
 }
