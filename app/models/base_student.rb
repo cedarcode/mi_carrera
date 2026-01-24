cab40a973efe8bd5a0614760374ff6501c389fb7
Renzo Minelli <renminelli@gmail.com>
2026-01-24 14:12:06 -0300

    scope credits to user degree

--- app/models/base_student.rb
+++ app/models/base_student.rb
@@ -30,10 +30,10 @@ class BaseStudent
   def available?(subject_or_approvable) = subject_or_approvable.available?(ids)
   def approved?(subject_or_approvable) = subject_or_approvable.approved?(ids)
   def group_credits(group) = group.subjects.approved_credits(ids)
-  def total_credits = Subject.approved_credits(ids)
+  def total_credits = degree.subjects.approved_credits(ids)
   def met?(prerequisite) = prerequisite.met?(ids)
   def group_credits_met?(group) = group_credits(group) >= group.credits_needed
-  def groups_credits_met? = SubjectGroup.all.all? { |group| group_credits_met?(group) }
+  def groups_credits_met? = degree.subject_groups.all? { |group| group_credits_met?(group) }
   def graduated? = total_credits >= 450 && groups_credits_met?
   def banner_viewed?(_) = raise NoMethodError
   def mark_banner_as_viewed!(_) = raise NoMethodError
