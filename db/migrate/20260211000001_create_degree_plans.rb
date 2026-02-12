dcc6a69747483b1e9b854f51c116e1c4e21966b9
Renzo Minelli <renminelli@gmail.com>
2026-02-12 20:03:49 -0300

    create index and foregin key inside create table macro

--- db/migrate/20260211000001_create_degree_plans.rb
+++ db/migrate/20260211000001_create_degree_plans.rb
@@ -1,13 +1,11 @@
 class CreateDegreePlans < ActiveRecord::Migration[8.1]
   def change
     create_table :degree_plans do |t|
-      t.string :degree_id, null: false
+      t.belongs_to :degree, null: false, foreign_key: true, index: false, type: :string
       t.string :name, null: false
       t.boolean :active, default: true, null: false
+      t.index %i[degree_id name], unique: true
       t.timestamps
     end
-
-    add_foreign_key :degree_plans, :degrees
-    add_index :degree_plans, [:degree_id, :name], unique: true
   end
 end
